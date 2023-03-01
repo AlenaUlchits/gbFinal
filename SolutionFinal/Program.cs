37052f5e16f5931f2cdbf558a52d6b1cc395aeba
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-03-01 11:11:07 +0300

    Find size for new array

---
+++ SolutionFinal/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(string[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.WriteLine("Write line ");
+        array[i] = Console.ReadLine();
+    }
+}
+void PrintArray(string[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine("\b\b  ");
+}
+int FindSizeOfNewArray(string[] array){
+    int size = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(array[i].Length <= 3){
+            size++;
+        }
+    }
+    return size;
+}
+int size = new Random().Next(1, 6);
+string[] array = new string[size];
+FillArray(array);
+PrintArray(array);
+int newSize = FindSizeOfNewArray(array);
+Console.WriteLine(newSize);
+
